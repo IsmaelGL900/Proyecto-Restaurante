a6d7f635046cfd71c042badf7c63f0cea1257063
ismaelgl <ismaelguerrerolopez11@gmail.com>
2025-04-02 20:49:08 +0200

    Segundo Guardado

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TodoAlRojo'
+    'TodoAlRojo',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
