ab946e0d041a27b5c4f72feff6a3464fefc62d4e
ismaelgl <ismaelguerrerolopez11@gmail.com>
2025-04-02 19:37:37 +0200

    Segundo Guardado

--- TodoAlRojo/urls.py
+++ TodoAlRojo/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     path('', go_home_page, name='home'),
     path('home/', go_home_page , name="home"),
+    path('mesas/', go_mesas_page, name='mesas'),
 ]
--- TodoAlRojo/views.py
+++ TodoAlRojo/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def go_home_page(request):
     return render(request, 'home.html')
+
+def go_mesas_page(request):
+    return render(request, 'mesas.html')
