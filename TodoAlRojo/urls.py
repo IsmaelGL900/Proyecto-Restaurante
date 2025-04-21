a7b63620a6ead2b40507b326b4e9ae1e5bcc5e1e
Chicharro <jchicharroortega@safareyes.es>
2025-04-21 15:59:58 +0200

    Logins Roles

--- TodoAlRojo/urls.py
+++ TodoAlRojo/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('home/login/', go_login_page, name="login"),
     path('home/login/registrarse/', go_registrarse_page, name="registrarse"),
     path('gestion/', go_gestion_page, name="gestion"),
+    path('gestion/loginCocinero/', go_loginCocinero_page, name="loginCocinero"),
+    path('gestion/loginCamarero/', go_loginCamarero_page , name="loginCamarero"),
+    path('gestion/loginAdministrador/', go_loginAdministrador_page, name="loginAdministrador"),
 ]
--- TodoAlRojo/views.py
+++ TodoAlRojo/views.py
@@ -18,3 +18,12 @@ def go_registrarse_page(request):
 
 def go_gestion_page(request):
     return render(request, 'Gestion.html')
+
+def go_loginCocinero_page(request):
+    return render(request, 'LoginCocinero.html')
+
+def go_loginCamarero_page(request):
+    return render(request, 'LoginCamarero.html')
+
+def go_loginAdministrador_page(request):
+    return render(request, 'LoginAdministrador.html')
