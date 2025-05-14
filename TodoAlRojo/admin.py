36765591bcb7cea923e8f59211e2086c6383a0f0
Chicharro <jchicharroortega@safareyes.es>
2025-05-14 17:22:50 +0200

    TodoUSUARIO 2

--- TodoAlRojo/admin.py
+++ TodoAlRojo/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+class UsuarioAdmin(UserAdmin):
+    model = Usuario
+    list_display = ('email', 'nombre', 'rol', 'is_staff', 'is_active')
+    list_filter = ('rol', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'nombre', 'rol', 'password')}),
+        ('Permisos', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nombre', 'rol', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(Usuario, UsuarioAdmin)
